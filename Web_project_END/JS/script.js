1aa7e7997dae1973aca06a2d418d0995a0519123
Sarawut <125782682+SarawutEz@users.noreply.github.com>
2024-02-29 13:56:21 +0700

    thumbnail

--- Web_project_END/JS/script.js
+++ Web_project_END/JS/script.js
@@ -3,10 +3,63 @@ function toggleham(x) {
 
   let myMenu = document.getElementById('myMenu');
   if (myMenu.className == 'menu') {
-      myMenu.className += ' menu_active'
+    myMenu.className += ' menu_active'
   }
   else {
     myMenu.className = 'menu'
   }
-
 }
+
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+// config param
+let countItem = items.length;
+let itemActive = 0;
+// event next click
+next.onclick = function () {
+  itemActive = itemActive + 1;
+  if (itemActive >= countItem) {
+    itemActive = 0;
+  }
+  showSlider();
+}
+//event prev click
+prev.onclick = function () {
+  itemActive = itemActive - 1;
+  if (itemActive < 0) {
+    itemActive = countItem - 1;
+  }
+  showSlider();
+}
+// auto run slider
+let refreshInterval = setInterval(() => {
+  next.click();
+}, 5000)
+function showSlider() {
+  // remove item active old
+  let itemActiveOld = document.querySelector('.slider .list .item.active');
+  let thumbnailActiveOld = document.querySelector('.thumbnail .item.active');
+  itemActiveOld.classList.remove('active');
+  thumbnailActiveOld.classList.remove('active');
+
+  // active new item
+  items[itemActive].classList.add('active');
+  thumbnails[itemActive].classList.add('active');
+
+  // clear auto time run slider
+  clearInterval(refreshInterval);
+  refreshInterval = setInterval(() => {
+    next.click();
+  }, 5000)
+}
+
+// click thumbnail
+thumbnails.forEach((thumbnail, index) => {
+  thumbnail.addEventListener('click', () => {
+    itemActive = index;
+    showSlider();
+  })
+})
